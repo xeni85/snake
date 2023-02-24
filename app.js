d4f0db1e4f6f31cf905db0dd6fdbaa196e74d9e3
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-02-24 09:39:46 -0500

    smoother motion, fixed the chain of motion

--- app.js
+++ app.js
@@ -1,105 +1,110 @@
-
 //variables
 
 let drawboard = document.querySelector(".can-vas");
-let snakeSpeed= 100;
+let snakeSpeed = 400;
 let gridSize = 21;
 let arrLength;
 let counter = 0;
 
-let snakeArr = 
+let snakeArr =
     // snake array
-    [
+    [{
+            // x: 11,
+            // y: 8,
+            x: Math.floor(gridSize / 2),
+            y: Math.ceil(gridSize / 2) - 3,
+        },
         {
-            x: 11,
-            y: 8,
-            // x: Math.floor(snake.variablePlace.gridSize/2), 
-            // y: Math.floor(snake.variablePlace.gridSize/2),
-        }, 
+            // x: 11,
+            // y: 9,
+            x: Math.floor(gridSize / 2),
+            y: Math.ceil(gridSize / 2) - 2,
+        },
         {
-            x: 11,
-            y: 9,
-            // x: Math.floor(snake.variablePlace.gridSize/2) - 1, 
-            // y: Math.floor(snake.variablePlace.gridSize/2),
-        }, 
+            // x: 11,
+            // y: 10,
+            x: Math.floor(gridSize / 2),
+            y: Math.ceil(gridSize / 2) - 1,
+        },
         {
-            x: 11,
-            y: 10,
-            // x: Math.floor(snake.variablePlace.gridSize/2) - 2, 
-            // y: Math.floor(snake.variablePlace.gridSize/2),
-        }, 
-        {
-            x: 11,
-            y: 11,
-            // x: Math.floor(snake.variablePlace.gridSize/2) - 3, 
-            // y: Math.floor(snake.variablePlace.gridSize/2),
-        }, 
+            // x: 11,
+            // y: 11,
+            x: Math.floor(gridSize / 2),
+            y: Math.ceil(gridSize / 2),
+        },
     ]
 
 
-    //create grid where snake will move
-    const createGrid = (drawGrid) => {
-        
-        // for (let i = 0; i < drawGrid ** 2; i++) {
-        //     snake.variablePlace.pixel = document.createElement('div');
-        //     snake.variablePlace.pixel.classList.toggle('pixel');
-        //     snake.variablePlace.drawboard.appendChild(snake.variablePlace.pixel);
-        // }
-        drawboard.style.gridTemplateColumns = `repeat(${drawGrid}, 1fr)`;
-        drawboard.style.gridTemplateRows = `repeat(${drawGrid}, 1fr)`;
-    };
+//create grid where snake will move
+const createGrid = (drawGrid) => {
+
+    // for (let i = 0; i < drawGrid ** 2; i++) {
+    //     snake.variablePlace.pixel = document.createElement('div');
+    //     snake.variablePlace.pixel.classList.toggle('pixel');
+    //     snake.variablePlace.drawboard.appendChild(snake.variablePlace.pixel);
+    // }
+    drawboard.style.gridTemplateColumns = `repeat(${drawGrid}, 1fr)`;
+    drawboard.style.gridTemplateRows = `repeat(${drawGrid}, 1fr)`;
+};
 
 
-    //create a snake 
-    const drawSnake = (arr) => {
-snakeArr.forEach(element => {
-                    const snakePart = document.createElement('div')
-                    snakePart.style.gridRowStart = element.x
-                    snakePart.style.gridColumnStart = element.y
-                    snakePart.classList.add("snake")
-                    arr.appendChild(snakePart)
-                    console.log ("This is the counter")
-});
-
-    }
-
-    //simulate motion
-    const updateFrames = () =>{
+//create a snake 
+const drawSnake = (arr) => {
+    snakeArr.forEach(element => {
         const snakePart = document.createElement('div')
-        snakePart.style.gridRowStart = snakeArr[counter].x
-        snakePart.style.gridColumnStart = snakeArr[counter].y
+        snakePart.style.gridRowStart = element.x
+        snakePart.style.gridColumnStart = element.y
         snakePart.classList.add("snake")
-        drawboard.appendChild(snakePart)
-        console.log ("This is the counter")
-        snakeArr[0].x += 0
-        snakeArr[0].y += 1
-        snakeArr.push({ ...snakeArr[0]});
-        snakeArr.splice(0, 1);
-        // console.log("this is the length" + arrLength)
-    }
+        arr.appendChild(snakePart)
+        console.log("This is the counter")
+    });
+
+}
+
+//simulate motion
+const updateFrames = () => {
+    const snakePart = document.createElement('div')
+    snakePart.style.gridRowStart = snakeArr[counter].x
+    snakePart.style.gridColumnStart = snakeArr[counter].y
+    snakePart.classList.add("snake")
+    drawboard.appendChild(snakePart)
+    console.log("This is the counter")
+    for (let i = snakeArr.length - 2; i >= 0; i--) {
+        snakeArr[i + 1] = { ...snakeArr[i] }
+      }
     
+      snakeArr[0].x += 0
+      snakeArr[0].y += 1
+        // snakeArr.push({
+        //     ...snakeArr[0]
+        // });
+        // snakeArr.splice(0, 1);
+}
+    // console.log("this is the length" + arrLength)
 
-    //game function
-    const game = () => {
 
-        setInterval(() => {
-            // for (let i = 0; i < arrLength - 1; i++) {
-            //     snakeArr.splice(arrLength - 2, 2);
-                
-            // }
-            drawboard.innerHTML = ''
-            createGrid(gridSize);
-            console.log(drawboard)
-            console.log("render");
-            // console.log("Game was called")
-            // snake.createGrid(21);
-            drawSnake(drawboard)
-            updateFrames();
-            // if (snakeArr.length >= 5) {
-            //     snakeArr = snakeArr.splice(-1);
-            // }
-        }, snakeSpeed);
-    }
+
+//game function
+const game = () => {
+
+    setInterval(() => {
+        // for (let i = 0; i < arrLength - 1; i++) {
+        //     snakeArr.splice(arrLength - 2, 2);
+
+        // }
+        drawboard.innerHTML = ''
+        createGrid(gridSize);
+        console.log(drawboard)
+        console.log("render");
+        // console.log("Game was called")
+        // snake.createGrid(21);
+        drawSnake(drawboard)
+        updateFrames();
+        // if (snakeArr.length >= 5) {
+        //     snakeArr = snakeArr.splice(-1);
+        // }
+    }, snakeSpeed);
+}
 
 
 //call main function
